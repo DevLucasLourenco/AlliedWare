526f717da57bc85c3dabbab88e5840fe4049da22
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-07-25 19:43:02 -0300

    att

---
+++ GUI/tutorial/tutorial.py
@@ -0,0 +1,6 @@
+# colocar este tutorial acima do local de ver logs.
+#pensar em uma maneira boa de apresentar, talvez fazer em word e salvar em pdf
+# pra ser mostrado quando clicar no botão
+# - pdf mais escuro, com fundo preto puxando pro cinza 
+# (se pa da pra fzr pelo notion e ) expoertar em pdf pra usar esse 
+# (fzr por la pra ficar bonito)
