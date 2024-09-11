a6efa74ab9f33a7928f76d484524f4c4aae17864
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-09-11 11:36:08 -0300

    main update

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from src.GUI.GUI import GUIMain
 
-
 if __name__ == "__main__":
     app = GUIMain()
     app.master.mainloop()
-
-# trocar para puxar
-# tudo, não só PDFs
-
 
 # testar streamlit
 
